1665141c758108dde584818928790633d9b7054c
Patrick <pat.rodriguez108@gmail.com>
2017-12-12 15:27:28 -0600

    Create interval class

--- interval.rb
+++ interval.rb
@@ -1,13 +1,21 @@
+class Interval
+	attr_accessor :start, :end
+
+	def initialize(s=0, e=0)
+	    @start = s
+	    @end = e
+	end
+end
+
 def insert(intervals, new_interval)
-    
+	p intervals
 end
 
 # example_one
 
+intervals = [Interval.new(1, 3), Interval.new(6, 9)]
 
-intervals = [[1,3], [6,9]]
-
-new_interval = [2,5]
+new_interval = Interval.new(2, 5)
 
 insert(intervals, new_interval)
 # -> [1,5],[6,9]
