bafa6a1c81a0bb39a71827a12b423af2ba3ebb65
Patrick <pat.rodriguez108@gmail.com>
2017-12-16 09:03:45 -0600

    Attempt to convert to arrays of ranges

--- interval.rb
+++ interval.rb
@@ -12,15 +12,21 @@ class Interval
 end
 
 def insert(intervals, new_interval)
-	intervals.each do |range|
-		if range.start > new_interval.start
-			range.start = new_interval.start
-		end
-		if range.end < new_interval.end
-			range.end = new_interval.end
-		end
+	# intervals.each do |range|
+	# 	if range.start > new_interval.start
+	# 		range.start = new_interval.start
+	# 	end
+	# 	if range.end < new_interval.end
+	# 		range.end = new_interval.end
+	# 	end
+	# end
+	# p intervals
+	p new_interval_range = (new_interval.start..new_interval.end).to_a
+	intervals_ranges = intervals.map { |range| (range.start..range.end).to_a }
+	intervals_ranges.each do |range|
+		p range
+		# new_interval_range.include?(range)
 	end
-	p intervals
 end
 
 # example_one
@@ -32,8 +38,11 @@ new_interval = Interval.new(2, 5)
 insert(intervals, new_interval)
 # -> [1,5],[6,9]
 
-intervals = [Interval.new(1,2),Interval.new(3,5),Interval.new(6,7),Interval.new(8,10),Interval.new(12,16)]
+# example_two
+
+intervals = [Interval.new(1,2), Interval.new(3,5), Interval.new(6,7), Interval.new(8,10), Interval.new(12,16)]
 
 new_interval = Interval.new(4,9)
 
 insert(intervals, new_interval)
+# -> [1,2],[3,10],[12,16]
