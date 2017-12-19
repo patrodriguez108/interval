730e8d6f306306c6069b4ad58e5f0062f9fa40b3
Patrick <pat.rodriguez108@gmail.com>
2017-12-19 16:53:03 -0600

    Try new approach with first example

--- interval.rb
+++ interval.rb
@@ -14,25 +14,43 @@ class Interval
 end
 
 def insert(intervals, new_interval)
+	final_intervals = []
 	new_interval_range = new_interval.convert_to_range
-	p intervals_range = intervals.map { |interval| interval.convert_to_range }
+	intervals_range = intervals.map { |interval| interval.convert_to_range }
+	intervals_range.each do |interval|
+		new_interval_range.each do |check|
+			if interval.include?(check)
+				interval << new_interval_range
+			end
+		end
+		interval.uniq!
+		interval.each do |element|
+			if element.class == Array
+				interval.delete(element)
+				interval.concat(element)
+			end
+		end
+		interval.uniq!
+		final_intervals << Interval.new(interval[0], interval[-1])
+	end
+	p final_intervals
 end
 
 # example_one
 
-intervals = [Interval.new(1, 3), Interval.new(6, 9)]
+# intervals = [Interval.new(1, 3), Interval.new(6, 9)]
 
-new_interval = Interval.new(2, 5)
+# new_interval = Interval.new(2, 5)
 
-insert(intervals, new_interval)
+# insert(intervals, new_interval)
 # -> [1,5],[6,9]
 
 
 # example_two
 
-# intervals = [Interval.new(1,2), Interval.new(3,5), Interval.new(6,7), Interval.new(8,10), Interval.new(12,16)]
+intervals = [Interval.new(1,2), Interval.new(3,5), Interval.new(6,7), Interval.new(8,10), Interval.new(12,16)]
 
-# new_interval = Interval.new(4,9)
+new_interval = Interval.new(4,9)
 
-# insert(intervals, new_interval)
+insert(intervals, new_interval)
 # -> [1,2],[3,10],[12,16]
